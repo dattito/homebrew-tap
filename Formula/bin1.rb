5c60987a4284b6c297a6785f49eee6a7b076d903
I568850 <david.siregar@sap.com>
2024-10-07 16:40:27 -0700

    fix: wrong fields

--- Formula/bin1.rb
+++ Formula/bin1.rb
@@ -1,32 +1,12 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Bin1 < Formula
   desc ""
-  homepage ""
+  homepage "https://github.com/dattito/homebrew-tap"
   url "https://github.com/dattito/homebrew-tap/releases/download/bin1%400.6.0/bin1-aarch64-apple-darwin"
   sha256 "674ee9c6c5a38fcc7674ee2faf46adef7a593ce7e63fceed28caafed48e9fa12"
   license ""
-
-  # depends_on "cmake" => :build
+  version "0.6.0"
 
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test bin1`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    bin.install "bin1-aarch64-apple-darwin" => "bin1"
   end
 end
