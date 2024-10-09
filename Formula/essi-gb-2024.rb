1182f93196bfbb104ecb0066d11500849d1e1342
I568850 <david.siregar@sap.com>
2024-10-08 21:17:44 -0700

    feat: add essi-gb-2024 formula

---
+++ Formula/essi-gb-2024.rb
@@ -0,0 +1,8 @@
+# All Rights Reserved.
+#
+# This code cannot be used, reproduced, modified, or distributed without explicit written consent from the author.
+class EssiGb2024 < Formula
+  url "https://github.com/dattito/homebrew-tap/releases/download/cli_0.2.0/cli"
+  sha256 "b4e763c6c05d895aa4e642ae9d4e51543a98179256fb5e76dd6451a5e8ed7dc9"
+end
+
