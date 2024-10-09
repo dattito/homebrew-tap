e5ee4aa26218947b830a968032d15f935a84fb0b
David Siregar <ro6ji94w@duck.com>
2024-10-09 09:26:15 -0700

    essi-gb-2024 cli_0.2.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/essi-gb-2024.rb
+++ Formula/essi-gb-2024.rb
@@ -2,8 +2,8 @@
 #
 # This code cannot be used, reproduced, modified, or distributed without explicit written consent from the author.
 class EssiGb2024 < Formula
-  url "https://github.com/dattito/homebrew-tap/releases/download/cli_0.2.0/cli"
-  sha256 "b4e763c6c05d895aa4e642ae9d4e51543a98179256fb5e76dd6451a5e8ed7dc9"
+  url "https://github.com/dattito/homebrew-tap/releases/download/cli_0.2.2/cli"
+  sha256 "d70f7258ef7df6dfa024eca9c01501cd51cc850ad07356686f5f779c7c59adbf"
 
   def install
     bin.install "cli" => "essi-gb-2024"
