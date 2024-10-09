61a25c7f019b208660950574894a3125df316f7c
I568850 <david.siregar@sap.com>
2024-10-09 10:44:00 -0700

    doc: desc should start with capital letter

--- Formula/bin1.rb
+++ Formula/bin1.rb
@@ -1,5 +1,5 @@
 class Bin1 < Formula
-  desc "just testing stuff"
+  desc "Just testing stuff"
   homepage "https://github.com/dattito/homebrew-tap"
   url "https://github.com/dattito/homebrew-tap/releases/download/bin1%400.6.0/bin1-aarch64-apple-darwin"
   sha256 "674ee9c6c5a38fcc7674ee2faf46adef7a593ce7e63fceed28caafed48e9fa12"
--- Formula/essi-gb-2024.rb
+++ Formula/essi-gb-2024.rb
@@ -2,7 +2,7 @@
 #
 # This code cannot be used, reproduced, modified, or distributed without explicit written consent from the author.
 class EssiGb2024 < Formula
-  desc "birthday of a friend"
+  desc "Birthday of a friend"
   homepage "https://github.com/dattito/homebrew-tap"
   url "https://github.com/dattito/homebrew-tap/releases/download/cli_0.2.2/cli"
   sha256 "d70f7258ef7df6dfa024eca9c01501cd51cc850ad07356686f5f779c7c59adbf"
