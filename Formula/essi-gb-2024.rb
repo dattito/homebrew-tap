12bee388de9c6abf76e2639373a74a0bcf33510e
I568850 <david.siregar@sap.com>
2024-10-08 21:19:00 -0700

    fix: rename formula

--- Formula/essi-gb-2024.rb
+++ Formula/essi-gb-2024.rb
@@ -4,5 +4,9 @@
 class EssiGb2024 < Formula
   url "https://github.com/dattito/homebrew-tap/releases/download/cli_0.2.0/cli"
   sha256 "b4e763c6c05d895aa4e642ae9d4e51543a98179256fb5e76dd6451a5e8ed7dc9"
+
+  def install
+    bin.install "cli" => "essi-gb-2024"
+  end
 end
 
